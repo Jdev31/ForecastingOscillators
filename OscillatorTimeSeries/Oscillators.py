4b1a64d224847ff11574685504dc76526d9cfe4c
JamieHealy <76441360+Jdev31@users.noreply.github.com>
2025-07-27 17:59:46 +0100

    Updated Osccilator Code
    Error with self.epsilon - different definitions

--- OscillatorTimeSeries/Oscillators.py
+++ OscillatorTimeSeries/Oscillators.py
@@ -113,8 +113,62 @@ class Harmonic_Oscilator():
             return self.A * (np.cos(self.omega_drive * time - self.fp_const ) - np.exp(-self.gamma * time) * np.cos(self.omega_damped * time - self.fp_const ))
         else:
             return self.A_0 * np.exp(-self.gamma * time) * np.cos(self.omega_damped * time - np.pi/2) # + self.p_const 
-        
-oscillator = Oscillator(
+
+class Harmonic_Oscilataor2():
+    def __init__(self, mass, force_const, initial_amplitude, damping_const=0, driving_force=0, driving_frequency=0, velocity = 0):
+        self.m = mass
+        self.k = force_const
+        self.A = initial_amplitude  # Initial position (x0)
+        self.b = damping_const
+        self.F_0 = driving_force
+        self.omega_drive = driving_frequency
+        self.v = velocity
+        #self.p_const = init_phase_const
+        #self.eps = 1e-12
+
+        self.omega_0 = np.sqrt(self.k / self.m)  # Natural frequency
+        self.gamma = self.b / (2 * self.m)       # Damping coefficient
+        self.epsilon = np.sqrt(self.gamma ** 2 - self.omega_0 ** 2)
+
+        #self.omega_damped =  np.sqrt(self.omega_0 ** 2 - self.gamma ** 2)      
+        #self.overdamped = lambda: self.gamma > self.omega_0 + self.eps
+        self.overdamped = lambda: self.b**2/(4 * self.m ** 2) > self.omega_0 ** 2
+        self.crit_damped = lambda: self.omega_0 ** 2 == self.b**2/(4 * self.m ** 2)
+        self.light_damped = lambda:  self.b**2/(4 * self.m ** 2) < self.omega_0 ** 2
+
+        if self.overdamped: 
+            self.C_1 = (self.A * (self.epsilon - self.gamma) + self.v)/(2 * self.epsilon)
+            self.C_2 = (self.A * (self.epsilon + self.gamma) - self.v)/(2 * self.epsilon)
+        elif self.crit_damped:
+            self.C_1 = self.A
+            self.C_2 = self.v - self.gamma * self.A
+        elif self.light_damped:
+            self.C_1 = self.A
+            self.C_2 = (self.v - self.gamma * self.A)/self.epsilon
+
+        self.D_1 = 0.0
+        self.D_2 = 0.0
+
+        if (self.F_0 > 0):
+            self.D_1 = (self.F_0 * self.omega_drive) / ((self.k - self.omega_drive ** 2 * self.m) ** 2 + (self.omega_drive * self.b) ** 2)
+            self.D_2 = (self.F_0 * (self.k - self.omega_drive ** 2 * self.m))/((self.omega_drive * self.b) ** 2 + (self.k - self.omega_drive ** 2 * self.m) ** 2)
+
+    def get_x_pos(self, time):
+        self.x_damped = 0.0
+        if (self.overdamped):
+            self.x_damped = self.C_1 * np.exp((self.gamma + self.epsilon) * time) + self.C_2 * np.exp((self.gamma - self.epsilon) * time)
+        elif (self.crit_damped):
+            self.x_damped = np.exp(self.gamma * time) * (self.C_1 + self.C_2 * time)
+        elif (self.light_damped):
+            self.x_damped = np.exp(self.gamma * time) * (self.C_1 * np.cos(self.epsilon * time) + self.C_2 * np.sin(self.epsilon * time))
+
+        self.x_forced = self.D_1 * np.sin(self.omega_drive * time) + self.D_2 * np.cos(self.omega_drive * time)
+
+        self.x_pos = self.x_damped + self.x_forced
+        return self.x_pos
+
+
+oscillator = Harmonic_Oscilataor2(
     mass=1,
     force_const=1,
     initial_amplitude=10,
